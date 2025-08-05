5e393d6cd7796c372cdbf8687f4d2d5ad325160e
Максим <maksim@MacBook-Air-Maksim.local>
2025-08-05 17:05:02 +0500

    Изначально была загружена не та папка

---
+++ test/main.py
@@ -0,0 +1,29 @@
+import requests
+import json
+
+BASE_URL = "https://nspd.gov.ru"
+SEARCH_ENDPOINT = "/api/geoportal/v2/search/geoportal"
+
+params={
+    "query": "66:41:0401035",#"66:41:0505001:497",
+    "thematicSearchId": 1,
+}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    "Accept": "application/json",
+}
+
+response = requests.get(
+    url=BASE_URL + SEARCH_ENDPOINT,
+    params=params,
+    headers=headers,
+    verify=False
+)
+
+if response.status_code == 200:
+    output_file = "nspd_result.geojson"
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(response.json(), f, ensure_ascii=False, indent=4)
+else:
+    print(f"Ошибка: {response.status_code}")
+    print(response.text)
